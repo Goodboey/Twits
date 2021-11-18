d4752f722637419348ad182f5788744f38d02745
goodboey <seb_p@hotmail.com>
2021-11-18 20:32:31 +0100

    ayo fler billeder mate

--- main.php
+++ main.php
@@ -72,110 +72,120 @@ if (isset($_SESSION['uid']) & isset($_SESSION['password'])) {
     <div class='col-lg-9' style='border:darkslateblue; border-style: solid; overflow-y: auto; max-height: 100vh'>
         <?php
         if ($loggedtrue == true){
-            echo "<div class='col-lg-12' style='border:grey; border-style: solid;'> 
-            <form action='main.php' method=post enctype='multipart/form-data'>
+            echo "<div class= row style='border:grey; border-style: solid; width: relative;'> 
+            <form action='main.php' method=post enctype='multipart/form-data' style='width: relative'>
             <table>
                 <tr>
                     <td>Titel:</td>
+                </tr>
+                <tr>
                     <td><input type='text' name='ptitle'></td>
                 </tr>
                 <tr>
-                    <td>Vis mig dine tanker:</td>
-                    <td><input type='text' name='pcontent'></td>
+                    <td><textarea type='text' name='pcontent' rows='6' cols='100' width='100%' placeholder='Hvad har du på hjerte?' style = 'resize: none;'></textarea></td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td><input type='file' name='picture' value='Upload et billede!'></td>
+                    <td><input type='file' name='picture[]' value='Upload et billede!' multiple='multiple'></td>
                 </tr>
                 <tr>
-                    <td></td>
                     <td><input type='submit' name='submitbtn' value='Post!'></td>
                 </tr>
             </table>
             </form>
             </div>";
+            // Hvis brugeren trykker på submitknap og ihvertfald har udfyldt opslag og titel, så lader vi brugeren poste opslaget.
             if (isset($_POST['submitbtn']) & isset($_POST['ptitle']) & isset($_POST['pcontent'])) {
-            
+                // tilføjer opslaget og henter post id til en variabel
                 $pid = add_post($_SESSION['uid'], $_POST['ptitle'], $_POST['pcontent']);
+                // hvis brugeren har tilføjet et billede tilføjer vi billedet til postet.
                 if (isset($_FILES['picture']['type'])){
-
-                    $temptype = substr($_FILES['picture']['type'], strpos($_FILES['picture']['type'],'/')+1);
-                    $iid = add_image($_FILES['picture']['tmp_name'], $temptype);
+                    // vi finder filtyperne for alle billeder og rykker dem til den rigtige placering på serveren
+                    // herefter tilføjer vi billedet til opslaget.
+                    // vi itererer over alle filoverførelser, hvor i er mindre end mængden af billeder..
+                    for ($i=0; $i < count($_FILES['picture']['name']); $i++){
+                        
+                        $tempFilePath = $_FILES['picture']['tmp_name'][$i]; // Vi tager den temp fil-sti fra det i'de billede
+                        $tempType = substr($_FILES['picture']['type'][$i], strpos($_FILES['picture']['type'][$i],'/')+1); // Vi kigger på filtypen og gemmer den til en enkel streng
+                        $iid = add_image($tempFilePath, $tempType); // vi tilføjer billedet, og tilskriver det en variabel da den returnerer image-ID.
                     
-                    if ($iid > 0){
-                        move_uploaded_file($_FILES['picture']['tmp_name'], $_FILES['picture']['name']);
-                        add_attachment($pid, $iid);
-                    }
+                        if ($iid > 0){ // Check om billede id'et er større end 0, måske lidt dumt skrevet men det sørger for at spøgelses billeder ikke kommer med.
+                            $newFilePath = $_FILES['picture']['name'][$i]; //Vi finder den nye filpath under name
+                            move_uploaded_file($tempFilePath, $newFilePath); //Vi flytter den uploadede fil til den nye filepath
+                            add_attachment($pid, $iid); //Vi tilføjer billedet til det korresponderende post
+                        }
+                    }               
                 }
                 
-                header("Refresh:0");
+                header("Refresh:0"); // Vi refresher siden så brugeren kan se sit nye post
             }
         }
 
-        $pids = get_pids();
-        foreach ($pids as $pid) {
+        $pids = get_pids(); // vi fanger post id'erne fra databasen
+        foreach ($pids as $pid) { // for hver post tager vi posts ud til et array
             $posts[] = get_post($pid);
         }
-        $order = array();
-        foreach ($posts as $key => $value) {
-            $order[] = strtotime($value['date']);
+        $order = array(); // laver et ordensarray
+        foreach ($posts as $post) { // Vi gemmer alle datoerne til vores ordensarray
+            $order[] = $post['date'];
         }
-        array_multisort($order, SORT_DESC, $posts);
+        array_multisort($order, SORT_DESC, $posts); // multisort tager et ordens array, med nøgler (datoer) og sorterer datoerne i descending order så vi har de nyeste posts først.
         foreach ($posts as $post) {
             $user = get_user($post['uid']);  //Vi benytter brugerid'et fra posten til at finde info om forfatteren.
-            if($loggedtrue){
-                echo "<div onclick='location.href=".'"viewpost.php?pid='.$post['pid'].'"'.";' style='cursor: pointer;'>";
-            } else {
-                echo "<div>";
-            }
-            //echo "<a href='postview.php' class='fill-div'></a>";
+            
             //Titel og forfatter
-            echo "<div class='row'>";
+            
+            echo "<div class= 'row' style='border:solid chartreuse; border-style:outset; width: relative'>";
+ 
 
-            echo "<div class='col-lg-12' style='background-color:#FF8300;'><h2>" . htmlentities($post['title']) . "</h2></div>" . "<h3>skrevet af: <a href=\"" . "user.php?uid=" . $user['uid'] . "\" >" .  htmlentities($user['firstname']) . " " . htmlentities($user['lastname']) . "</a></h3><div>". $post['date'] ."</div>";
+            // Vi anvender bootstrap grid til at udforme vores post - "tidslinje"
+            if($loggedtrue){
+                echo "<div onclick='location.href=".'"viewuser.php?uid='.$post['uid'].'"'.";' style='cursor: pointer;'>"; // hvis man er logget ind kan man trykke på postet
+            } else {
+                echo "<div>"; // hvis man ikke er logget ind laver vi bare en tom åben-div, den bliver afsluttet ligemeget hvad.
+            }
+            echo "<div class='col-lg-12' style='background-color:#FF8300;'><h2>" . htmlentities($post['title']) . "</h2></div>" . 
+                 "<h3>skrevet af: <a href=\"" . "user.php?uid=" . $user['uid'] . "\" >" .  htmlentities($user['firstname']) . " " . htmlentities($user['lastname']) . "</a></h3><div>". $post['date'] ."</div>";
+            
+            echo "</div>";
+                 
             //Titlen bliver skrevet og et link bliver lagt ind til brugerens side med forfatterens navn
-
+            if($loggedtrue){
+                echo "<div onclick='location.href=".'"viewpost.php?pid='.$post['pid'].'"'.";' style='cursor: pointer;'>"; // hvis man er logget ind kan man trykke på postet
+            } else {
+                echo "<div>"; // hvis man ikke er logget ind laver vi bare en tom åben-div, den bliver afsluttet ligemeget hvad.
+            }
             //Indhold
             echo "<p><div class='col-lg-12' style='border:thick; border-style: groove; background-color: white' >" . htmlentities($post['content']) . "</div></p>";
 
             //Billeder
 
+            
+
             $images = get_iids_by_pid($post['pid']); //Vi henter alle de billed id'er som er knyttet til posten.
+            
             echo "<div class = 'row'>";
             foreach ($images as $iid) { //for hvert billede knyttet til posten tilføjer vi et html img tag med billedets path.
                 $path = get_image($iid)['path'];
 
-                echo "<div class = 'image-wrapper' style='max-width: 500px;'><div style='padding: 0 0 60%'><div><img class='rounded float-right' src=\"" . $path . "\"" . "></div></div></div>";
+                echo "<div class = 'image-wrapper' style='max-width: 400px;'><div style='padding: 0 0 60%'><div>
+                      <img class='rounded float-right' src=\"" . $path . "\"" . "></div></div></div>";
             }
             echo "</div>";
-            echo "</div>";
+            
 
             //Kommentarer
             $comments = get_cids_by_pid($post['pid']); //Vi henter et array af comment ids til oplæggets id
             foreach ($comments as $cid) { //Vi kører igennem arrayet for hvert kommentar ID
 
-                if($toomanycomments > 4){
-                    //echo "<div><h5> 'Vis alle kommentarer' </h5></div>";
-                    $toomanycomments = 0;
-                    //break;
-                }
                 echo "<section style= 'border:thin; border-style: solid; background-color: white'>";
                 $comment = get_comment($cid); //Vi henter information om den enkelte kommentar fra databasen.
                 //Hvorefter vi indsætter et link til forfatterens. Og derefter kommentarens indhold.
-                echo "<h5><a href=\"" . "user.php?uid=" . $comment['uid'] . "\" >" . htmlentities($comment['uid'])  . "</a>";
-
-                echo "</h5>";
-
+                echo "<h5><a href=\"" . "user.php?uid=" . $comment['uid'] . "\" >" . htmlentities($comment['uid'])  . "</a></h5>";
                 echo "<p style = 'overflow-wrap: anywhere;'>" . htmlentities($comment['content']) . "</p>";
                 echo "</section>";
-                $toomanycomments++;
-
-                if ($toomanycomments == count($comments)){
-                    $toomanycomments = 0;
-                }
 
             }
-            echo "</div><div class='row'> <br> </div>";
+            echo "</div></div><div class='row'> <br> </div>";
         }
 
 
